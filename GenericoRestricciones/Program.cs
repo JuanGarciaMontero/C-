8181fb0c5321717f207f7b616396c5e3e0ab182f
Juan García Montero <jgmen20@gmail.com>
2022-09-06 19:36:03 +0200

    Clases Genericas y Restricciones

---
+++ GenericoRestricciones/Program.cs
@@ -0,0 +1,129 @@
+﻿using System;
+
+namespace GenericoRestricciones
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            AlmacenEmpleados<Secretaria> empleados = new AlmacenEmpleados<Secretaria>(3);
+
+            empleados.agregar(new Secretaria(4500));
+
+            empleados.agregar(new Secretaria(1500));
+
+            empleados.agregar(new Secretaria(2500));
+
+
+            // Clase generica con restricciones ya que no tiene el declarado el método salario
+            // obligado de la interface, (tampoco tiene Estudiante heradado la Interface)
+            // Da error en el momento de compilación.
+
+            AlmacenEmpleados<Estudiante> empleados = new AlmacenEmpleados<Estudiante>(3);
+
+            empleados.agregar(new Estudiante(4500));
+
+            empleados.agregar(new Estudiante(1500));
+
+            empleados.agregar(new Estudiante(2500));
+
+
+        }
+    }
+
+    class AlmacenEmpleados<T> where T: IparaEmpleado
+    {
+
+        public AlmacenEmpleados(int z)
+        {
+            datosEmpleado = new T[z];
+        }
+
+        public void agregar(T obj)
+        {
+            datosEmpleado[i] = obj;
+
+            i++;
+        }
+
+        public T getEmpleado(int i)
+        {
+            return datosEmpleado[i];
+        }
+
+        private int i = 0;
+
+        private T[] datosEmpleado;
+    }
+
+    class Director: IparaEmpleado
+    {
+
+        public Director(double salario)
+        {
+            this.salario = salario;
+        }
+
+        private double salario;
+
+        public double getSalario()
+        {
+            return salario;
+        }
+    }
+
+    class Secretaria:IparaEmpleado
+    {
+        public Secretaria(double salario)
+        {
+            this.salario = salario;
+        }
+
+        public double getSalario()
+        {
+            return salario;
+        }
+
+        private double salario;
+
+
+    }
+
+    class Electricista:IparaEmpleado
+    {
+        public Electricista(double salario)
+        {
+            this.salario = salario;
+        }
+
+        public double getSalario()
+        {
+            return salario;
+        }
+
+        private double salario;
+
+    }
+
+    class Estudiante
+    {
+        public Estudiante(double edad)
+        {
+            this.edad = edad;
+        }
+
+        public double getEdad()
+        {
+            return edad;
+        }
+
+        private double edad;
+    }
+
+
+    interface IparaEmpleado
+    {
+        double getSalario();
+    }
+}
---
+++ Genericos/Program.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Genericos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // AlmacenObjetos archivos = new AlmacenObjetos(4);
+
+            //AlmacenObjetos<Empleado> archivos = new AlmacenObjetos<Empleado>(4);
+
+            // AlmacenObjetos<String> archivos = new AlmacenObjetos<String>(4);
+
+            AlmacenObjetos<DateTime> archivos = new AlmacenObjetos<DateTime>(4);
+
+            archivos.agregar(new DateTime());
+
+            archivos.agregar(new DateTime());
+
+            archivos.agregar(new DateTime());
+
+            archivos.agregar(new DateTime());
+
+            /*archivos.agregar("Juan");
+
+            archivos.agregar("Elena");
+
+            archivos.agregar("Antonio");
+
+           /* archivos.agregar("Sandra");
+
+
+            /*archivos.agregar(new Empleado(1500));
+
+            archivos.agregar(new Empleado(2500));
+
+            archivos.agregar(new Empleado(3500));
+
+            archivos.agregar(new Empleado(4500));*/
+
+
+            //String nombrePersona = (String)archivos.getElemento(2);
+
+            //Empleado salarioEmpleado = (Empleado)archivos.getElemento(2);
+
+
+            /*Empleado salarioEmpleado = archivos.getElemento(2);
+
+            Console.WriteLine(salarioEmpleado.getSalario());*/
+
+            
+            /*String nombrePersona = archivos.getElemento(2);
+
+            Console.WriteLine(nombrePersona);*/
+
+            DateTime nombrePersona = archivos.getElemento(2);
+
+            Console.WriteLine(nombrePersona);
+
+            
+    }
+    }
+
+    class AlmacenObjetos<T>
+    {
+        public AlmacenObjetos(int z)
+        {
+            datosElemento = new T[z];
+        }
+
+        public void agregar( T obj)
+        {
+            datosElemento[i] = obj;
+
+            i++;
+        }
+
+        public T getElemento(int i)
+        {
+            return datosElemento[i];
+        }
+
+        private T [] datosElemento;
+
+        private int i = 0;
+    }
+
+    /*class AlmacenObjetos
+    {
+        public AlmacenObjetos(int z)
+        {
+            datosElemento = new object[z];
+        }
+
+        public void agregar(Object obj)
+        {
+            datosElemento[i] = obj;
+
+            i++;
+        }
+
+        public Object getElemento(int i)
+        {
+            return datosElemento[i];
+        }
+
+        private Object[] datosElemento;
+
+        private int i = 0;
+    }*/
+
+    class Empleado
+    {
+        public Empleado(double salario)
+        {
+            this.salario = salario;
+        }
+
+        public double getSalario()
+        {
+            return salario;
+        }
+
+        private double salario;
+    }
+}
